55b14a35136c23884d0341286992b12d738033c2
Hữu Tình <121339897+Tinhdev061@users.noreply.github.com>
2023-08-29 14:58:29 +0700

    Delete FlapyBird directory

--- FlapyBird/FlapyBirdSDL/BackGround.cpp
+++
@@ -1,7 +0,0 @@
-#include "BackGround.h"
-void Background::BackGround_Update() {
-    Background_PosX -= Background_MovingSpeed;
-    if (Background_PosX <= 0) {
-        Background_PosX = 500;
-    }
-}
--- FlapyBird/FlapyBirdSDL/GLOBAL_DATA.h
+++
@@ -1,7 +0,0 @@
-#ifndef DATA_H
-#define DATA_H
-#include <SDL.h>
-extern SDL_Window* Sceen;
-extern SDL_Renderer* GRenderer;
-extern SDL_Rect gSpriteClips[4];
-#endif
